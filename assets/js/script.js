5dc5d5a645b63c4df6aa7904eab4cd8efcd8dd39
Emily N Mayeski <enmayeski@gmail.com>
2021-06-13 23:18:51 -0400

    Added innterhtml to needed elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,106 @@
 // QUESTIONS ARRAY
 var questions = [
-    {question: "What does HTML stand for?", 
-    answers: [
-        {text: "HyperText Markup Language", correct: true},
-        {text: "HyperText Markup Logistics", correct: false},
-        {text: "Hand Typed Markup Language", correct: false},
-        {text: "HyperText Modal Language", correct: false}
-    ]
-}
-];
+    {
+        question: "What does HTML stand for?", 
+        a: "HyperText Markup Language",
+        b: "HyperText Markup Logistics", 
+        c: "Hand Typed Markup Language",
+        d: "HyperText Modal Language",
+        correct: "a"
+    },
+    {
+        question: "What does CSS stand for?", 
+        a: "Cool Style Sheets",
+        b: "Cascading Sheeted Style", 
+        c: "Cassandra Style Sheets",
+        d: "Cascading Style Sheets",
+        correct: "d"
+    },
+    {
+        question: "What does JS stand for?", 
+        a: "JumpScript",
+        b: "JavaScript", 
+        c: "JuneScript",
+        d: "JavaStyles",
+        correct: "b"
+    }
+]
 
 // GET NEEDED ELEMENTS FOR MANIPULATION
 var questionEl = document.getElementById("question");
 var timerEl = document.getElementById("countdown");
 var scoreEl = document.getElementById("score");
-var quizContent = document.getElementById("quiz-content");
+var quizContent = document.getElementById("quiz-content").classList.add("hide");
 var startBtn = document.getElementById("start-btn");
 var showBtn = document.getElementById("show-btn");
+var ansBtn = document.getElementsByClassName("btn");
+var choiceA = document.getElementById("btnA");
+var choiceB = document.getElementById("btnB");
+var choiceC = document.getElementById("btnC");
+var choiceD = document.getElementById("btnD");
+
+// START AND SHOW QUIZ SCORE AT 0
+var quizScore = 0;
+scoreEl.innerText = quizScore;
+
+for (var i = 0; i < questions.length; i++) {
+    questionEl.innerHTML = questions[i].question;
+    choiceA.innerHTML = "A: " + questions[i].a;
+    choiceB.innerHTML = "B: " + questions[i].b;
+    choiceC.innerHTML = "C: " + questions[i].c;
+    choiceD.innerHTML = "D: " + questions[i].d;
+
+}
+
+
+// SHOW QUESTIONS
+
+// var showQuestions = function() {
+//     for (var i = 0; i < questions.length; i++) {
+
+//         var userAnswer = document.addEventListener("click", function(){
+//             choiceA.innerHTML = "Hello World";
+//           });
+          
+//             // User answer is whatever button the user picks
+//             console.log(userAnswer);
+//         }
+        
+//         if (userAnswer === questions[i].correct) {
+//             quizScore++;
+//         } else {
+//             timeLeft = timeLeft - 25;
+//         }
+//     } 
+
+
+
 
 
 // START QUIZ FUNCTION
 var startQuiz = function() {
     document.getElementById("start-btn").classList.add("hide");
+    document.getElementById("quiz-content").classList.toggle("hide");
     countdown();
+    // showQuestions();
 }
 
 
 //END QUIZ FUNCTIOn
 
 var endQuiz = function() {
-    quizContent.classList.toggle("hide");
-    startBtn.classList.toggle("hide");
+    document.getElementById("start-btn").classList.toggle("hide");
+    document.getElementById("quiz-content").classList.toggle("hide");
 }
 
 
-// START AND SHOW QUIZ SCORE AT 0
-var quizScore = 0;
-scoreEl.innerText = quizScore;
 
 // timer - GOOD
 var timerEl = document.getElementById("timer");
 
+var timeLeft = 1000;
+
 var countdown = function() {
-    var timeLeft = 10;
     var timeInterval = setInterval(function() {
         if (timeLeft >= 0) {
             timerEl.textContent = timeLeft + "s";
